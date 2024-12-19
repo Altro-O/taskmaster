b76000ec51479677eaf43ba66b9ea577f36c494f
Altro-O <asdordotacs@gmail.com>
2024-12-19 04:03:55 +0300

    fix: export GameService class instead of instance

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -1,6 +1,19 @@
 const { User } = require('../models');
 
 class GameService {
+    constructor() {
+        this.achievementTypes = {
+            TASKS_COMPLETED: {
+                levels: [5, 20, 50, 100],
+                points: [10, 50, 100, 200]
+            },
+            PRIORITY_MASTER: {
+                levels: [5, 15, 30, 50],
+                points: [20, 60, 120, 200]
+            }
+        };
+    }
+
     async getUserLevel(userId) {
         try {
             const user = await User.findOne({
@@ -23,4 +36,4 @@ class GameService {
     }
 }
 
-module.exports = new GameService();
+module.exports = GameService;
