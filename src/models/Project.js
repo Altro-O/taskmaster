991c91a4bedd788d71008ff988800464e9f598b2
Altro-O <asdordotacs@gmail.com>
2024-12-21 19:34:04 +0300

    fix: database configuration - Fix Project model definition - Add sequelize configuration - Fix DataTypes import

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,20 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
-    const Project = sequelize.define('Project', {
-        title: DataTypes.STRING,
-        description: DataTypes.TEXT,
-        status: {
-            type: DataTypes.ENUM('TODO', 'IN_PROGRESS', 'IN_REVIEW', 'DONE'),
-            defaultValue: 'TODO'
-        },
-        deadline: DataTypes.DATE
-    });
+module.exports = (sequelize) => {
+    class Project extends Model {
+        static init(sequelize) {
+            return super.init({
+                title: {
+                    type: DataTypes.STRING,
+                    allowNull: false
+                },
+                description: DataTypes.TEXT,
+                status: {
+                    type: DataTypes.ENUM('TODO', 'IN_PROGRESS', 'IN_REVIEW', 'DONE'),
+                    defaultValue: 'TODO'
+                },
+                deadline: DataTypes.DATE
+            }, {
+                sequelize,
+                modelName: 'Project'
+            });
+        }
 
-    Project.associate = function(models) {
-        Project.belongsTo(models.User);
-        Project.hasMany(models.Task);
-    };
+        static associate(models) {
+            Project.belongsTo(models.User);
+            Project.hasMany(models.Task);
+        }
+    }
 
+    Project.init(sequelize);
     return Project;
 }; 
